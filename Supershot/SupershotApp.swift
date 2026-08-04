5c5735e43054cf984053a866ed4c6ca9ed5d1b06
J <jacob@lapworth.nz>
2026-08-04 13:52:35 +0100

    Add RevenueCat

--- Supershot/SupershotApp.swift
+++ Supershot/SupershotApp.swift
@@ -1,9 +1,11 @@
 import SwiftUI
 import SQLiteData
 import ComposableArchitecture
+import RevenueCat
 
 @main struct SupershotApp: App {
   init() {
+    Purchases.configure(withAPIKey: "test_zKRQzbkGIeFdTqByFkpdStXjvQI")
     try! prepareDependencies {
       try $0.bootstrapDatabase()
 #if DEBUG
